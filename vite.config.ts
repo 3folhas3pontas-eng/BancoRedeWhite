8dd4ecfd680936a2d4864de765bb291d9ba0e636
3folhas3pontas-eng <3folhas3pontas@gmail.com>
2026-01-25 23:17:56 -0300

    feat: Configure Vite build output
    Explicitly set the `input` for `rollupOptions` to ensure correct asset handling during the build process. Also sets the `base` path to '/'.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -9,5 +8,11 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-  }
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+  base: '/',
 });
